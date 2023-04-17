36e7443e961f2e5e3a7cd19885c766b139939da9
Hunter Spack <hunterspack@gmail.com>
2023-04-17 00:00:48 -0400

    fixed the precision issue

--- GPACalculator/pch.cpp
+++ GPACalculator/pch.cpp
@@ -33,21 +33,20 @@ string GPACalculator::teacherResponse(string teacherName)
 // have more material to work with in "Brian Mode"
 string GPACalculator::respondToGPA(float gpa)
 {
-	cout << gpa << "\n";
 	if (getIsBrianMode()){
 		if (gpa == 4.0) {
 			return("Wow, you must be a genius or whater!");
 		}
-		else if (gpa <= 3.9 && gpa >= 3.0) {
+		else if (gpa < 4.0 && gpa >= 3.0) {
 			return("Excellent work, overachiever!");
 		}
-		else if (gpa <= 2.9 && gpa >= 2.0) {
+		else if (gpa < 3.0 && gpa >= 2.0) {
 			return("Good job, but let's be honest, we both know you can do better.");
 		}
-		else if (gpa <= 1.9 && gpa >= 1.0) {
+		else if (gpa < 2.0 && gpa >= 1.0) {
 			return("Looks like you've been enjoying college life a little too much.");
 		}
-		else if (gpa <= 0.9 && gpa > 0) {
+		else if (gpa < 1.0 && gpa > 0) {
 			return("You never go to class what did you expect?");
 		}
 		else if (gpa == 0) {
@@ -58,16 +57,16 @@ string GPACalculator::respondToGPA(float gpa)
 		if (gpa == 4.0) {
 			return("A PERFECT GPA!!!!");
 		}
-		else if (gpa <= 3.9 && gpa >= 3.0) {
+		else if (gpa < 4.0 && gpa >= 3.0) {
 			return("Excellent! You have a GPA in the A range.");
 		}
-		else if (gpa <= 2.9 && gpa >= 2.0 ) {
+		else if (gpa < 3.0 && gpa >= 2.0 ) {
 			return("Good job! You have a GPA in the B range.");
 		}
-		else if (gpa <= 1.9 && gpa >= 1.0) {
+		else if (gpa < 2.0 && gpa >= 1.0) {
 			return("You have a GPA in the C range. Keep working hard!");
 		}
-		else if (gpa <= 0.9 && gpa > 0) {
+		else if (gpa < 1.0 && gpa > 0) {
 			return("Your GPA is in the D range. You may need to improve.");
 		}
 		else if (gpa == 0) {
